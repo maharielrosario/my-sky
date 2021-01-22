08fab7e51efdc10f710d0b0e595d074caabbe08a
Mahariel <12755283+mr0sari0@users.noreply.github.com>
2021-01-22 17:23:28 -0500

    move weather funtionality to root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CityWeatherData, WeatherData, HTTPErrorData } from './interfaces';
+import { determineInputType, replaceSpacesForApi, fToC, cToF } from './util';
+import { WeatherService } from './weather.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   appName = 'My Sky';
+  weatherData: CityWeatherData;
+  searchValue = '';
+  tempScale = 'Celsius';
+  error: string;
+
+  constructor(private weatherService: WeatherService) {}
+
+  getWeather(): void {
+    if (this.error) {
+      this.error = '';
+    }
+    if (this.searchValue) {
+      const apiInputValue = replaceSpacesForApi(this.searchValue);
+      const inputType = determineInputType(this.searchValue);
+      this.weatherService
+        .getWeather(apiInputValue, inputType, this.tempScale)
+        .subscribe(
+          (response: WeatherData) => {
+            const { data } = response;
+            this.weatherData = data[0];
+            const roundedTemp = this.weatherData.temp.toFixed();
+            this.weatherData.temp = parseInt(roundedTemp, 10);
+            this.searchValue = '';
+          },
+          (error: HTTPErrorData) => {
+            this.error = error.message;
+          }
+        );
+    } else {
+      this.error = 'No city entered';
+    }
+  }
+  updateSearchValue(newSearchValue: string): void {
+    this.searchValue = newSearchValue;
+  }
+  updateTempScale(): void {
+    if (this.tempScale === 'Celsius') {
+      if (this.weatherData) {
+        this.weatherData.temp = cToF(this.weatherData.temp);
+      }
+      this.tempScale = 'Fahrenheit';
+    } else {
+      if (this.weatherData) {
+        this.weatherData.temp = fToC(this.weatherData.temp);
+      }
+      this.tempScale = 'Celsius';
+    }
+  }
 }
--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,7 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { WeatherService } from '../weather.service';
-import { determineInputType, replaceSpacesForApi, fToC, cToF } from '../util';
-import { CityWeatherData, WeatherData, HTTPErrorData } from '../interfaces';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CityWeatherData } from '../interfaces';
 
 @Component({
   selector: 'app-cities',
@@ -10,50 +8,20 @@ import { CityWeatherData, WeatherData, HTTPErrorData } from '../interfaces';
 })
 export class CitiesComponent {
   @Input() appName: string;
-  @Input() searchValue = '';
-  @Input() tempScale = 'Celsius';
-  weatherData: CityWeatherData;
-  error: string;
-  constructor(private weatherService: WeatherService) {}
-  getWeather(): void {
-    if (this.error) {
-      this.error = '';
-    }
-    if (this.searchValue) {
-      const apiInputValue = replaceSpacesForApi(this.searchValue);
-      const inputType = determineInputType(this.searchValue);
-      this.weatherService
-        .getWeather(apiInputValue, inputType, this.tempScale)
-        .subscribe(
-          (response: WeatherData) => {
-            const { data } = response;
-            this.weatherData = data[0];
-            const roundedTemp = this.weatherData.temp.toFixed();
-            this.weatherData.temp = parseInt(roundedTemp, 10);
-            this.searchValue = '';
-          },
-          (error: HTTPErrorData) => {
-            this.error = error.message;
-          }
-        );
-    } else {
-      this.error = 'No city entered';
-    }
+  @Input() searchValue: string;
+  @Input() tempScale: string;
+  @Input() weatherData: CityWeatherData;
+  @Output() getWeather: EventEmitter = new EventEmitter();
+  @Output() updateSearchValue: EventEmitter = new EventEmitter();
+  @Output() updateTempScale: EventEmitter = new EventEmitter();
+
+  getTheWeather(): void {
+    this.getWeather.emit(null);
   }
-  updateSearchValue(newSearchValue: string): void {
-    this.searchValue = newSearchValue;
+  updateTheSearchValue(newSearchValue: string): void {
+    this.updateSearchValue.emit(newSearchValue);
   }
-  updateTempScale(): void {
-    if (this.tempScale === 'Celsius') {
-      if (this.weatherData) {
-        this.weatherData.temp = cToF(this.weatherData.temp);
-      }
-      this.tempScale = 'Fahrenheit';
-    } else {
-      if (this.weatherData) {
-        this.weatherData.temp = fToC(this.weatherData.temp);
-      }
-      this.tempScale = 'Celsius';
-    }
+  updateTheTempScale(): void {
+    this.updateTempScale.emit(null);
   }
 }
