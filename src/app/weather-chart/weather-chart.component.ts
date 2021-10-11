1ed3ac3b020dc649cc9ed3889b4bf283357a16c5
MasterMahariel <mastermahariel@outlook.com>
2021-10-11 00:04:51 -0400

    create weather chart component

---
+++ src/app/weather-chart/weather-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'weather-chart',
+  templateUrl: './weather-chart.component.html',
+  styleUrls: ['./weather-chart.component.scss']
+})
+export class WeatherChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
