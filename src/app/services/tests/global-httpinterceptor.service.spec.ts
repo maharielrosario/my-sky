143e9db198f19935c0b461920f2335f2fe1db24e
MasterMahariel <mastermahariel@outlook.com>
2021-10-25 14:45:42 -0400

    add RouterTestingModule to http service test

--- src/app/services/tests/global-httpinterceptor.service.spec.ts
+++ src/app/services/tests/global-httpinterceptor.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { GlobalHTTPInterceptorService } from '../global-httpinterceptor.service';
 
@@ -6,7 +7,9 @@ describe('GlobalHTTPInterceptorService', () => {
   let service: GlobalHTTPInterceptorService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+    });
     service = TestBed.inject(GlobalHTTPInterceptorService);
   });
 
